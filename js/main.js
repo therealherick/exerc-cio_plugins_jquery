31202c551885820e2601c0d1b199a0e4afb3cfae
herick <hrckgomes@gmail.com>
2025-03-19 17:03:26 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,42 @@
+$('#telefone').mask('(00) 00000-0000', {
+    placeholder: '(DDD) 12345-6789'
+})
+
+$('#cpf').mask('000.000.000-00', {
+    placeholder: '123.456.789-00'
+})
+
+$('#cep').mask('00000-000', {
+    placeholder: '012345-678'
+})
+
+$('form').validate({
+    rules: {
+        nome: {
+            requerid: true
+        },
+        email: {
+            requerid: true,
+            email: true
+        },
+        telefone: {
+            requerid: true
+        },
+        endereco: {
+            requerid: true
+        },
+        cep: {
+            requerid: true
+        },
+        cpf: {
+            requerid: true
+        },
+    },
+    submitHandler: function (form) {
+        alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
+        form.reset();
+    },
+    invalidHandler: function (form, validator) {
+        alert("Por favor, preencha os campos para prosseguir com a compra!");
+    }
+})
